031976f8f2f216aa62130bc2fa51679bc366e76a
daiyanabdulaziz <111733143+daiyanabdulaziz@users.noreply.github.com>
2022-09-19 19:09:28 +0700

    Add files via upload

---
+++ IF.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int nilai = 78;
+	if(nilai > 78){
+		cout << "Lulus" << endl;
+	  }else{
+	  	cout << "Tidak Lulus" << endl;
+	 } 	
+return 0;	
+}
